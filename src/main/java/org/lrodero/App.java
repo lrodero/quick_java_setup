62be070dfadb338efd5a145afcb658a492bea7b3
Luis Rodero-Merino <luis.rod@gmail.com>
2016-12-18 19:44:00 +0100

    Added exampe with Observable

--- src/main/java/org/lrodero/App.java
+++ src/main/java/org/lrodero/App.java
@@ -1,6 +1,8 @@
 package org.lrodero;
 
 import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 
 
@@ -16,12 +18,31 @@ public class App {
 
   public static void hello(String... names) {
     Observable.fromArray(names).subscribe(new Consumer<String>() {
-
       @Override
       public void accept(String s) {
         System.out.println("Hello " + s + "!");
       }
+    });
 
+    System.out.println();
+    
+    Observable.fromArray(names).subscribe(new Observer<String>() {
+      @Override
+      public void onComplete() {
+        System.out.println("Completed");
+      }
+      @Override
+      public void onError(Throwable e) {
+        System.out.println("Error: " + e);
+      }
+      @Override
+      public void onNext(String s) {
+        System.out.println("Hello " + s + "!!");
+      }
+      @Override
+      public void onSubscribe(Disposable d) {
+        System.out.println("Subscribed with disposable " + d);
+      }
     });
   }
 }
