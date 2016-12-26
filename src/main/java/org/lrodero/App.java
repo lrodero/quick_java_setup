bbcc47a30e786e0dfdb07034735cf14f59933232
Luis Rodero-Merino <luis.rod@gmail.com>
2016-12-26 20:01:15 +0100

    Added log4j2 for logs, keep working on examples

--- src/main/java/org/lrodero/App.java
+++ src/main/java/org/lrodero/App.java
@@ -5,11 +5,18 @@ import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Hello world application, adapted to RxJava 2
  */
 public class App {
+
+  private final static Logger log = LogManager.getFormatterLogger(App.class);
 
   public static void main(String[] args) {
     hello("this","is","a","test");
@@ -17,32 +24,39 @@ public class App {
 
 
   public static void hello(String... names) {
-    Observable.fromArray(names).subscribe(new Consumer<String>() {
+
+    final Observable<String> o = Observable.fromArray(names);
+
+    o.subscribe(new Consumer<String>() {
       @Override
       public void accept(String s) {
-        System.out.println("Hello " + s + "!");
+        log.info("Accepting '%s'", s);
       }
     });
 
-    System.out.println();
-    
-    Observable.fromArray(names).subscribe(new Observer<String>() {
+    o.subscribe(new Observer<String>() {
       @Override
       public void onComplete() {
-        System.out.println("Completed");
+        log.info("Completed");
       }
       @Override
       public void onError(Throwable e) {
-        System.out.println("Error: " + e);
+        log.info("Error: " + e);
       }
       @Override
       public void onNext(String s) {
-        System.out.println("Hello " + s + "!!");
+        log.info("Hello " + s + "!!");
       }
       @Override
       public void onSubscribe(Disposable d) {
-        System.out.println("Subscribed with disposable " + d);
+        log.info("Subscribed with disposable " + d);
       }
     });
+
+    final Observable<Integer> o2 = Observable.range(1,20);
+    final Observable<Integer> o3 = o2.skip(10).take(5).map(i -> i * 1000);
+    o2.subscribe(i -> log.info(i));
+    o3.subscribe(i -> log.info(i));
+
   }
 }
