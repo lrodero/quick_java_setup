29d3f150131afca4ed48bcf87e42dc0ab6d2aade
Luis Rodero-Merino <luis.rod@gmail.com>
2017-01-07 19:47:47 +0100

    Modified example Java file

---
+++ src/main/java/org/lrodero/Experiment.java
@@ -0,0 +1,12 @@
+package org.lrodero;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public final class Experiment {
+
+  public final static void main(String[] args) {
+    System.out.println("Now to start working...");
+  }
+
+}
